f22b48c2229a5c3f6b172fe4f01dd467079545ce
Jasper Kennis <jasper@creativetoken.nl>
2011-10-17 16:15:14 +0200

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Strippekaar::Application.config.secret_token = 'ef80ff10cb9d76dd916896ba77ade3e264b1eae9230589056391d50a2be3ae1b228f613a2a6833537907679a55fb359867e4c63c3064db7963a3e6a44a5b31c7'
