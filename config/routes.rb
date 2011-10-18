9e027a5e01407774dc9f3a9d84c89ed14a50a88b
Jasper Kennis <jasper@creativetoken.nl>
2011-10-18 14:12:24 +0200

    Created the route to /users, greening the second step.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Strippekaar::Application.routes.draw do
   get "user/index"
 
   devise_for :users, :path_names => {:sign_in => 'login'}
+  
+  match 'users' => 'user#index'
 
   root :to => "user#index"
   
@@ -52,10 +54,6 @@ Strippekaar::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
