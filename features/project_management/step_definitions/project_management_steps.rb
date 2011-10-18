2d764d278c8b950f443cd89e1ec30aef0657b629
Jasper Kennis <jasper@creativetoken.nl>
2011-10-18 17:09:44 +0200

    Wrote simple implementations of all steps. Not all steps might be correct, but we can work on that from here.

--- features/project_management/step_definitions/project_management_steps.rb
+++ features/project_management/step_definitions/project_management_steps.rb
@@ -17,27 +17,36 @@ Given /^I am logged in as "([^"]*)" with "([^"]*)" rights$/ do |login, role|
 end
 
 Given /^I am on the projects overview$/ do
-  pending # express the regexp above with the code you wish you had
+  @first_project = Project.create!(
+    :name => 'First project'
+  )
+  
+  @second_project = Project.create!(
+    :name => 'Second project'
+  )
+  visit '/projects'
 end
 
 Then /^I should see all projects$/ do
-  pending # express the regexp above with the code you wish you had
+  page.should have_content 'First project'
+  page.should have_content 'Second project'
 end
 
 Then /^I should have the option to add a new project$/ do
-  pending # express the regexp above with the code you wish you had
+  page.should have_content 'Add new project'
 end
 
 Given /^I am on the new project page$/ do
-  pending # express the regexp above with the code you wish you had
+  visit '/projects/new'
 end
 
 When /^I complete the new project form$/ do
-  pending # express the regexp above with the code you wish you had
+  fill_in 'project_name', :with => "First project"
+  click_button 'Create project' 
 end
 
 Then /^I should see a confirmation message$/ do
-  pending # express the regexp above with the code you wish you had
+  page.should have_content '"First project" has been created'
 end
 
 Then /^I should redirect to the project page$/ do
@@ -45,25 +54,43 @@ Then /^I should redirect to the project page$/ do
 end
 
 Given /^I am on a project page$/ do
-  pending # express the regexp above with the code you wish you had
+  @first_project = Project.create!(
+    :name => 'First project'
+  )
+  visit path_to @first_project
 end
 
 When /^I remove the project$/ do
-  pending # express the regexp above with the code you wish you had
+  click_link "Delete project"
 end
 
 When /^I am on a project page$/ do
-  pending # express the regexp above with the code you wish you had
+  @first_project = Project.create!(
+    :name => 'First project'
+  )
+  visit path_to @first_project 
 end
 
 Then /^I should have the option to add users$/ do
-  pending # express the regexp above with the code you wish you had
+  page.should have_content 'Add new user'
 end
 
 Then /^I should see users$/ do
-  pending # express the regexp above with the code you wish you had
+  @first_project = Project.create!(
+    :name => 'First project'
+  )
+  @first_porject_user = User.create!(
+    :login => 'FirstUser',
+    :password => 'generic',
+    :password_confirmation => 'generic',
+    :email => "FirstUser@example.com",
+    :role => 'user'
+  )
+  @first_project_user.project = @first_project
+  
+  page.should have_content @first_project_user.name
 end
 
 Then /^I should have the option to remove the project$/ do
-  pending # express the regexp above with the code you wish you had
+  page.should have_content 'Delete project'
 end
